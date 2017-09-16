3e13acd8598fe1159ec4a72c0a8543db74f8e74e
Youness Hakky <youness.hakky@gmail.com>
2017-09-16 22:07:37 +0100

    adding bytes to Editor func

--- scripts/script.js
+++ scripts/script.js
@@ -87,25 +87,41 @@
 
 })();
 
-
-
 (function (window) {
   var document = window.document;
 
   // editor class
 
-  function Editor () {}
+  function Editor (canvas, opts) {
+    if (canvas) {
+      if (typeof canvas == 'string') {
+        this.canvas = getElm(canvas)
+      }else {
+        this.canvas = canvas
+      }
+      this.context = this.canvas.getContext('2d')
+    }
+  }
 
 
   Editor.prototype.make = function () {
-    console.log('hello Editor.js')
+    console.log('hello from HELL!')
+  }
+
+  Editor.prototype.setImage = function (img) {
+    var self = this;
+
   }
 
   window.Editor = Editor
 
 })(this)
 
-// -- hole new era
+// how this class is used
+// var ed = new Editor(mainCanvas, {
+//   canvas: mainCanvas
+// })
+
 
 function makeCanva(id) {
   var canvas = document.createElement('canvas');
