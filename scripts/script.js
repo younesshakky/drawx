4c33433dfb64ba8c2e9614453a36cac734aea8b2
youness hakky <youness.hakky@gmail.com>
2017-09-17 00:31:50 +0000

    fix merge errors

--- scripts/script.js
+++ scripts/script.js
@@ -88,32 +88,6 @@
 })();
 
 
-
-(function (window) {
-  var document = window.document;
-
-  // editor class
-
-  function Editor () {}
-
-  // experimental method
-  Editor.prototype.log = function () {
-    console.log('hello Editor.js')
-  }
-
-  window.Editor = Editor
-
-})(this)
-
-// -- hole new era
-
-function makeCanva(id) {
-  var canvas = createElement('canvas', {
-    id: id
-  })
-
-  return canvas
-}
 // general purpose functions
 
 // is (http/s) set
